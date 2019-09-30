bc4ac6a391a6b76f8b142bcdc8732318d0a4508b
KseniiaMazan <ksusamazan@gmail.com>
2019-09-30 18:35:06 +0300

    Updates

---
+++ precache-manifest.9beb8d890cdea1733f242c8c8110c117.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82e336c53edb9a553300cba511f4898d",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "4d2e0c54685b1006f8e7",
+    "url": "/react_tabs-with-router/static/css/main.ca2d2164.chunk.css"
+  },
+  {
+    "revision": "48bc3d0da9127736332f",
+    "url": "/react_tabs-with-router/static/js/2.73d33912.chunk.js"
+  },
+  {
+    "revision": "4d2e0c54685b1006f8e7",
+    "url": "/react_tabs-with-router/static/js/main.0020f6b2.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
